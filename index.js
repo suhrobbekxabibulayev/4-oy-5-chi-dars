785d504845a91e887fc5a4a16b4e00e3a2f976cd
Suhrob <Suhrob@gmail.com>
2024-12-29 16:17:27 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,106 @@
+// function sum(x, y) {
+//     return x + y;
+// }
+// console.log(sum(3, 2)); 
+// console.log(sum(-3, -6)); 
+// console.log(sum(7, 3)); 
+
+
+// function toSekunds(minutes) {
+//     return minutes * 60;
+// }
+// console.log(toSekunds(5));
+// console.log(toSekunds(3));
+// console.log(toSekunds(2));
+
+// function nextInteger(int) {
+//     return int + 1;
+// }
+// console.log(nextInteger(0));
+// console.log(nextInteger(9));
+// console.log(nextInteger(-3)); 
+
+// function uchburchakYuzi(asos, balandlik) {
+//     return (asos * balandlik) / 2;
+// }
+// console.log(uchburchakYuzi(3, 2)); 
+// console.log(uchburchakYuzi(7, 4)); 
+// console.log(uchburchakYuzi(10, 10));
+
+// function ageToDays(age) {
+//     return age * 365;
+// }
+// console.log(ageToDays(65)); 
+// console.log(ageToDays(0)); 
+// console.log(ageToDays(20)); 
+
+// function kub(x) {
+//     return x ** 3;
+// }
+// console.log(kub(3)); 
+// console.log(kub(5)); 
+// console.log(kub(10)); 
+
+// function firstElement(arr) {
+//     return arr[0];
+// }
+// console.log(firstElement([1, 2, 3])); 
+// console.log(firstElement([80, 5, 100])); 
+// console.log(firstElement([-500, 0, 50])); 
+
+// function power(x, y) {
+//     return x * y;
+// }
+// console.log(power(230, 10));
+// console.log(power(110, 3)); 
+// console.log(power(480, 20)); 
+
+// function hourToSekunds(hour) {
+//     return hour * 3600;
+// }
+// console.log(hourToSekunds(2)); 
+// console.log(hourToSekunds(10)); 
+// console.log(hourToSekunds(24)); 
+
+// function uchinchiTomon(tomon1, tomon2) {
+//     return tomon1 + tomon2 - 1;
+// }
+// console.log(uchinchiTomon(8, 10)); 
+// console.log(uchinchiTomon(5, 7)); 
+// console.log(uchinchiTomon(9, 2)); 
+
+// function qoldiq(son1, son2) {
+//     return son1 % son2;
+// }
+// console.log(qoldiq(1, 3)); 
+// console.log(qoldiq(3, 4)); 
+// console.log(qoldiq(-9, 45)); 
+// console.log(qoldiq(5, 5)); 
+
+// function turtburchakYuzi(boyi, eni) {
+//     return boyi * eni;
+// }
+// console.log(turtburchakYuzi(6, 7)); 
+// console.log(turtburchakYuzi(20, 10)); 
+// console.log(turtburchakYuzi(2, 9)); 
+
+// function stringQoshish(a) {
+//     return "something " + a;
+// }
+// console.log(stringQoshish("is better than nothing")); 
+// console.log(stringQoshish("Bob Jane")); 
+// console.log(stringQoshish("something")); 
+
+// function kvadrat(x) {
+//     return x ** 2;
+// }
+// console.log(kvadrat(5)); 
+// console.log(kvadrat(9)); 
+// console.log(kvadrat(100)); 
+
+// function noldan(raqam) {
+//     return raqam <= 0;
+// }
+// console.log(noldan(5)); 
+// console.log(noldan(0)); 
+// console.log(noldan(-2)); 
